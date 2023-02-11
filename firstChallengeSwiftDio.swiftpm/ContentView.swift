03700ebda1bb4db9313a21843ce6e533d02fe36e
Matheus Mendes <matheus_mendess@live.com>
2023-02-11 14:44:56 -0300

    Finalizando o primeiro desafio

--- firstChallengeSwiftDio.swiftpm/ContentView.swift
+++ firstChallengeSwiftDio.swiftpm/ContentView.swift
@@ -6,12 +6,26 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Primeiro desafio Swift Dio.me!")
+            Text("Primeiro desafio Swift Dio.me! \n")
             
-            let firstName = "Steve"
-            var lastName: String? = "Jobs"
             
-            Text("\(firstName) \(lastName ?? "Wozniak")")
+            Text("Brincado com Tuplas")
+            let founder = ("Steve", "Jobs", "Steve", "Wozniak", "Ronald", "Wayne")
+            
+            let tuplaFounder = founder
+            
+            Text("Fundador \(tuplaFounder.0) \(tuplaFounder.1)")
+            Text("Co-fundador \(tuplaFounder.2) \(tuplaFounder.3)")
+            Text("Co-fundador \(tuplaFounder.4) \(tuplaFounder.5) \n")
+            
+            Text("Desafio DIO.me ")
+            let firstName = "Steve" // Constante Steve
+            var lastName: String? = "Jobs" // Variavel Opcional do tipo String com valor Job
+            Text("\(firstName) \(lastName ?? "Wozniak")") // Printando a constante e a variavel e passando um valor default "Wozniak"
+            /*
+             O if optional binding na variavel e printando o valor desembrulhado
+             da consta e da variavel desembrulhada
+             */
             
             if let unwrapSecondName = lastName {
                 Text("\(firstName) \(unwrapSecondName)")
